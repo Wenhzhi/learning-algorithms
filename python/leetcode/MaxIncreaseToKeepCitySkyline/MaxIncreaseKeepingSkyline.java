d5492bc027bb8d2db0352043f233f0f4ddb558f7
Wenhzhi <53692498+Wenhzhi@users.noreply.github.com>
2021-12-13 01:08:58 +0800

    807. Max Increase to Keep City Skyline
    https://leetcode-cn.com/problems/max-increase-to-keep-city-skyline/

---
+++ python/leetcode/MaxIncreaseToKeepCitySkyline/MaxIncreaseKeepingSkyline.java
@@ -0,0 +1,73 @@
+import com.eclipsesource.json.JsonArray;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Solution {
+    public int maxIncreaseKeepingSkyline(int[][] grid) {
+        int n = grid.length;
+        int[] rowMax = new int[n];
+        int[] colMax = new int[n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                rowMax[i] = Math.max(rowMax[i], grid[i][j]);
+                colMax[j] = Math.max(colMax[j], grid[i][j]);
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                ans += Math.min(rowMax[i], colMax[j]) - grid[i][j];
+            }
+        }
+        return ans;
+    }
+}
+
+
+class MainClass {
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return new int[0];
+        }
+
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for(int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+
+    public static int[][] stringToInt2dArray(String input) {
+        JsonArray jsonArray = JsonArray.readFrom(input);
+        if (jsonArray.size() == 0) {
+            return new int[0][0];
+        }
+
+        int[][] arr = new int[jsonArray.size()][];
+        for (int i = 0; i < arr.length; i++) {
+            JsonArray cols = jsonArray.get(i).asArray();
+            arr[i] = stringToIntegerArray(cols.toString());
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            int[][] grid = stringToInt2dArray(line);
+
+            int ret = new Solution().maxIncreaseKeepingSkyline(grid);
+
+            String out = String.valueOf(ret);
+
+            System.out.print(out);
+        }
+    }
+}
